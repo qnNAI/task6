9ffa8b12954639836aaee2f91369163a049000f8
qnnai <qnnai@gmail.com>
2023-05-30 16:13:13 +0300

    feat: messages notifications

--- task6/wwwroot/js/site.js
+++ task6/wwwroot/js/site.js
@@ -1,6 +1,26 @@
-﻿$(document).ready(function () {
+﻿let hubConnection;
+
+$(document).ready(function () {
     $('#f-recipient').autocomplete({
         source: '/user/SearchAutocomplete',
         delay: 500
     });
+
+    hubConnection = new signalR.HubConnectionBuilder()
+        .withUrl("/chat")
+        .build();
+
+    hubConnection.on("Receive", receiveMsg)
 });
+
+function sendMessage() {
+    let sub = document.getElementById('');
+    let recipient = document.getElementById('');
+    let content = document.getElementById('');
+
+    hubConnection.invoke("Send", sub, content, recipient);
+}
+
+function receiveMsg(subject, content, recipient, sender) {
+
+}
